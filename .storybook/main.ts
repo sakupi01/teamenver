18de667d63472ef65411e46c46099ade889fa852
saku-1101 <sakupi1101@outlook.jp>
2023-08-01 23:29:11 +0900

    add: テスト環境整備 #14

---
+++ .storybook/main.ts
@@ -0,0 +1,38 @@
+import type { StorybookConfig } from '@storybook/nextjs'
+import path from 'path'
+const config: StorybookConfig = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-onboarding',
+    '@storybook/addon-interactions',
+    '@storybook/addon-a11y',
+    {
+      name: '@storybook/addon-styling',
+      options: {
+        postCss: {
+          implementation: require.resolve('postcss'),
+        },
+      },
+    },
+  ],
+  webpackFinal: async (config) => {
+    // storybook/nextでts.configのpathを使用する設定
+    if (config.resolve) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        '@': path.resolve(__dirname, '../src'),
+      }
+    }
+    return config
+  },
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+}
+export default config
