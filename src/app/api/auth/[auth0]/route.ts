693b6be388c1454b8ac3afc438dc0ff0cab43d81
saku-1101 <sakupi1101@outlook.jp>
2023-08-01 12:57:21 +0900

    add: (認証テスト)Hasura Cloud-Auth0接続 close #4

---
+++ src/app/api/auth/[auth0]/route.ts
@@ -0,0 +1,27 @@
+import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
+
+export const GET = handleAuth({
+  login: handleLogin({
+    authorizationParams: {
+      prompt: "login",
+    },
+    returnTo: "/",
+  }),
+  signup: handleLogin({
+    authorizationParams: {
+      prompt: "login",
+      screen_hint: "signup",
+    },
+    returnTo: "/",
+  }),
+});
+
+/*
+This exported SDK creates the following routes:
+
+/api/auth/login: The route used to perform login with Auth0.
+/api/auth/logout: The route used to log the user out.
+/api/auth/callback: The route Auth0 will redirect the user to after a successful login.
+/api/auth/me: The route to fetch the user profile from.
+
+*/
