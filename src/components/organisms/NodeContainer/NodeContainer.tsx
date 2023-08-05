31acf50ae350245b623c3beafb6fabf72d7c9369
saku-1101 <sakupi1101@outlook.jp>
2023-08-05 15:19:01 +0900

    bugfix: #25 iframe htmlが読み込めないとかではなく，swcを使用せずにturbopackを使用していたためだった．理由詳細は不明！

--- src/components/organisms/NodeContainer/NodeContainer.tsx
+++ src/components/organisms/NodeContainer/NodeContainer.tsx
@@ -2,37 +2,42 @@
 import { WebContainer } from '@webcontainer/api'
 import { useEffect } from 'react'
 
-import { files } from '@/app/files'
+import { files } from '@/app/webContainerSideFiles'
 
 import style from './NodeContainer.module.css'
 
 /** @type {import('@webcontainer/api').WebContainer}  */
 let webcontainerInstance: WebContainer
 
+const loader = async () => {
+  // Call only once
+  webcontainerInstance = await WebContainer.boot()
+  await webcontainerInstance.mount(files)
+  console.log('here')
+
+  const packageJSON = await webcontainerInstance.fs.readFile('package.json', 'utf-8')
+  console.log(packageJSON)
+
+  const installProcess = await webcontainerInstance.spawn('npm', ['install'])
+
+  installProcess.output.pipeTo(
+    new WritableStream({
+      write(data) {
+        console.log(data)
+      },
+    }),
+  )
+
+  const textareaEl = document!.querySelector('textarea')
+  if (textareaEl != null) {
+    textareaEl.value = files['index.js'].file.contents
+  }
+}
+
 export const NodeContainer = () => {
   useEffect(() => {
-    window.addEventListener('load', async () => {
-      // Call only once
-      webcontainerInstance = await WebContainer.boot()
-      await webcontainerInstance.mount(files)
-
-      const packageJSON = await webcontainerInstance.fs.readFile('package.json', 'utf-8')
-      console.log(packageJSON)
-      const installProcess = await webcontainerInstance.spawn('npm', ['install'])
-
-      installProcess.output.pipeTo(
-        new WritableStream({
-          write(data) {
-            console.log(data)
-          },
-        }),
-      )
-
-      const textareaEl = document!.querySelector('textarea')
-      if (textareaEl != null) {
-        textareaEl.value = files['index.js'].file.contents
-      }
-    })
+    window.addEventListener('load', loader)
+    return () => document.removeEventListener('load', loader)
   }, [])
 
   return (
@@ -41,7 +46,7 @@ export const NodeContainer = () => {
         <textarea className={style.textarea} defaultValue='Start Coding!'></textarea>
       </div>
       <div className='preview'>
-        <iframe src='/loading.html'></iframe>
+        <iframe src='/loading'></iframe>
       </div>
     </div>
   )
