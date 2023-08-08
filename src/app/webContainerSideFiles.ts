337355e1180806defa314c3056417d74a7081d07
saku-1101 <sakupi1101@outlook.jp>
2023-08-08 20:53:09 +0900

    add: #25 スクリプトベースでvite app作成から外部ライブラリインストールまで

--- src/app/webContainerSideFiles.ts
+++ src/app/webContainerSideFiles.ts
@@ -4,7 +4,36 @@ export const files: FileSystemTree = {
   'index.js': {
     file: {
       contents: `
-      // 🌸 Here's the full code which is going to be executed in the terminal.
+      // 🚀 You can just type \`node index.js\` to start your Vite app with your settings!
+      // In order to observe what contents were generated, stop the server with \`ctrl + C\` and type \`ls\` on the terminal.
+
+      // 🧞‍♂️ Psst! here's the full code which is going to be executed in the terminal.
+
+      // *********************************************
+
+
+      const { execSync } = require('child_process');
+
+      try {
+        // 1. Create a Vite project using Vue template
+        execSync('pnpm create vite my-vue-app --template vue', { stdio: 'inherit' });
+
+        // Change to the project directory
+        process.chdir('my-vue-app');
+
+        // 2. Install @pandacss/dev as a development dependency
+        execSync('pnpm install -D @pandacss/dev', { stdio: 'inherit' });
+
+        // 3. Initialize PandaCSS with postcss
+        execSync('pnpm panda init --postcss', { stdio: 'inherit' });
+        
+        console.log('All commands executed successfully.');
+        
+        execSync('pnpm run dev', { stdio: 'inherit' });
+      } catch (error) {
+        console.error('An error occurred:', error.message);
+      }
+
       `,
     },
   },
