aaeb8d3b073191d03ec586f0bfdcf5df811817cc
saku-1101 <sakupi1101@outlook.jp>
2023-09-27 09:17:45 +0900

    feat: change the routing according to where the user was before the session expires

---
+++ src/app/select/team/page.tsx
@@ -0,0 +1,24 @@
+import { redirect } from 'next/navigation'
+
+import { createTeam } from '@/services/server/CreateTeam'
+import { css } from 'styled-system/css'
+
+// million-ignore
+export default function Join() {
+  const createTeamHandler = async (data: FormData) => {
+    'use server'
+    const name = data.get(`name`) as string
+    const res = await createTeam({ name })
+    console.log(res)
+    redirect(`/select/board`)
+  }
+
+  return (
+    <main className={css({ padding: `5%` })}>
+      <form action={createTeamHandler} method='POST'>
+        <input type='text' name='name' placeholder='Input a team name' />
+        <button type='submit'>Create</button>
+      </form>
+    </main>
+  )
+}
