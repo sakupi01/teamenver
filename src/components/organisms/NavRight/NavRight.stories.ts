ecaa64ea8932628ac990a11cad8d506f971b698b
saku-1101 <sakupi1101@outlook.jp>
2023-10-12 21:28:39 +0900

    fix: add storybook args

--- src/components/organisms/NavRight/NavRight.stories.ts
+++ src/components/organisms/NavRight/NavRight.stories.ts
@@ -1,8 +1,7 @@
 import { Meta, StoryObj } from '@storybook/react'
 import { within } from '@storybook/testing-library'
 
-import { NavRight } from './NavRight';
-
+import { NavRight } from './NavRight'
 
 const meta = {
   title: 'NavRight',
@@ -11,16 +10,17 @@ const meta = {
   parameters: {
     layout: 'fullscreen',
   },
-  argTypes: {
-  },
-} satisfies Meta<typeof NavRight>;
-
+  argTypes: {},
+} satisfies Meta<typeof NavRight>
 
 export default meta
 type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
-  play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement);
+  args: {
+    current_team_id: '0',
   },
-};
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+  },
+}
