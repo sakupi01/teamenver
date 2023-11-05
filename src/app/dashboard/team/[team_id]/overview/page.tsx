eee012efae735327d137de449a67c6d3faee8a1a
saku-1101 <sakupi1101@outlook.jp>
2023-11-05 16:39:24 +0900

    feat: マークダウンcrud

--- src/app/dashboard/team/[team_id]/overview/page.tsx
+++ src/app/dashboard/team/[team_id]/overview/page.tsx
@@ -96,9 +96,10 @@ import {
   ELEMENT_TH,
 } from '@udecode/plate-table'
 import { createTrailingBlockPlugin } from '@udecode/plate-trailing-block'
-import { useState } from 'react'
+import { useState, useTransition } from 'react'
 import { DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
+import useSWR from 'swr'
 
 import { BlockquoteElement } from '@/components/plate-ui/blockquote-element'
 import { Button } from '@/components/plate-ui/button'
@@ -139,6 +140,9 @@ import { withDraggables } from '@/components/plate-ui/with-draggables'
 import { Skeleton } from '@/components/ui/skeleton'
 import { toast } from '@/components/ui/use-toast'
 
+import * as GetProjectOverviewApi from '@/api/get/overview/route'
+
+import { handleProjectOverviewUpdate } from '@/libs/actions/handleProjectOverviewUpdate'
 const plugins = createPlugins(
   [
     createParagraphPlugin(),
@@ -346,109 +350,23 @@ const plugins = createPlugins(
   },
 )
 
-const initialValue = [
-  { type: 'h1', children: [{ text: '🌳 Blocks' }], id: '1' },
-  {
-    type: 'p',
-    children: [
-      {
-        text: 'Easily create headings of various levels, from H1 to H6, to structure your content and make it more organized.',
-      },
-    ],
-    id: '2',
-  },
-  {
-    type: 'blockquote',
-    children: [
-      {
-        text: 'Create blockquotes to emphasize important information or highlight quotes from external sources.',
-      },
-    ],
-    id: '3',
-  },
-  {
-    type: 'code_block',
-    lang: 'javascript',
-    children: [
-      {
-        type: 'code_line',
-        children: [{ text: '// Use code blocks to showcase code snippets' }],
-      },
-      { type: 'code_line', children: [{ text: 'function greet() {' }] },
-      // eslint-disable-next-line @stylistic/quotes
-      { type: 'code_line', children: [{ text: " console.info('Hello World!');" }] },
-      { type: 'code_line', children: [{ text: '}' }] },
-    ],
-    id: '4',
-  },
-  { type: 'h1', children: [{ text: '🌱 Marks' }], id: '1' },
-  {
-    type: 'p',
-    children: [
-      {
-        text: 'Add style and emphasis to your text using the mark plugins, which offers a variety of formatting options.',
-      },
-    ],
-    id: '2',
-  },
-  {
-    type: 'p',
-    children: [
-      { text: 'Make text ' },
-      { text: 'bold', bold: true },
-      { text: ', ' },
-      { text: 'italic', italic: true },
-      { text: ', ' },
-      { text: 'underlined', underline: true },
-      { text: ', or apply a ' },
-      { text: 'combination', bold: true, italic: true, underline: true },
-      { text: ' of these styles for a visually striking effect.' },
-    ],
-    id: '3',
-  },
-  {
-    type: 'p',
-    children: [
-      { text: 'Add ' },
-      { text: 'strikethrough', strikethrough: true },
-      { text: ' to indicate deleted or outdated content.' },
-    ],
-    id: '4',
-  },
-  {
-    type: 'p',
-    children: [
-      { text: 'Write code snippets with inline ' },
-      { text: 'code', code: true },
-      { text: ' formatting for easy readability.' },
-    ],
-    id: '5',
-  },
-  {
-    type: 'p',
-    children: [
-      { text: 'Press ' },
-      { text: '⌘+B', kbd: true },
-      { text: ' to apply bold mark or ' },
-      { text: '⌘+I', kbd: true },
-      { text: ' for italic mark.' },
-    ],
-    id: '6',
-  },
-]
-
 export default function OverviewPage() {
-  const { user, isLoading, error } = useUser()
+  const { user, isLoading } = useUser()
+  const [content, setContent] = useState<Value>([])
+  const [isPending, startTransition] = useTransition()
 
-  const [content, setContent] = useState<Value>(initialValue)
+  const fetcher = (url: string) => fetch(url).then((res) => res.json())
+  const res = useSWR<GetProjectOverviewApi.GetType>('/api/get/overview', fetcher)
+  const initVal: string = res.data?.project_details[0].project_abstract || ''
 
-  const handleSave = () => {
+  const handleSave = async () => {
+    const res = await handleProjectOverviewUpdate(content)
     toast({
       title: '✅ Your changes have been successfully saved.',
     })
   }
 
-  if (isLoading) {
+  if (isLoading || res.isLoading) {
     return (
       <div className='flex flex-col gap-4'>
         <Skeleton className='h-10 w-full' />
@@ -456,8 +374,8 @@ export default function OverviewPage() {
         <Skeleton className='h-10 w-full' />
       </div>
     )
-  } else if (error) {
-    return <div>{error.message}</div>
+  } else if (res.error) {
+    return <div>{res.error}</div>
   }
   return (
     <div className='flex flex-col items-end'>
@@ -465,7 +383,7 @@ export default function OverviewPage() {
         <CommentsProvider users={{}} myUserId={user?.sub}>
           <Plate
             plugins={plugins}
-            initialValue={initialValue}
+            initialValue={JSON.parse(initVal)}
             onChange={(newValue) => {
               setContent(newValue)
             }}
@@ -484,8 +402,8 @@ export default function OverviewPage() {
           </Plate>
         </CommentsProvider>
       </DndProvider>
-      <Button className='m-5' onClick={() => handleSave()}>
-        Save
+      <Button className='m-5' onClick={() => startTransition(() => handleSave())}>
+        {isPending ? 'loading...' : 'Save'}
       </Button>
     </div>
   )
