c52a328ba5fe9ff4170e92c52fa800919af0016c
saku-1101 <sakupi1101@outlook.jp>
2023-11-06 10:02:01 +0900

    style: チーム選択画面のui

--- src/app/select/team/page.tsx
+++ src/app/select/team/page.tsx
@@ -1,23 +1,40 @@
 'use client'
-
 import { useUser } from '@auth0/nextjs-auth0/client'
+import { zodResolver } from '@hookform/resolvers/zod'
 import { useRouter } from 'next/navigation'
+import { useForm } from 'react-hook-form'
 import useSWR from 'swr'
+import * as z from 'zod'
 
+import { Avatar, AvatarFallback } from '@/components/ui/avatar'
 import { Button } from '@/components/ui/button'
+import { Card, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { FormItem } from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area'
+import { Separator } from '@/components/ui/separator'
 
 import * as GetJoinedTeamsApi from '@/api/get/joined_teams/route'
 
 import { createTeamHandler } from '@/libs/actions/createTeamHandler'
 import { handleSetCookies } from '@/libs/actions/handleSetCookies'
 
+const formSchema = z.object({
+  teamName: z.string().min(2).max(25),
+})
+
 // million-ignore
 export default function Join() {
   const { user, isLoading, error } = useUser()
   const router = useRouter()
-  // fetch created teams and update the team_member of that if the user selects the team from select box
 
-  // fetch the teams that the user already joined in and redirect them to the designated team page.
   const fetcher = (url: string) =>
     fetch(url).then((res) => {
       return res.json()
@@ -37,32 +54,80 @@ export default function Join() {
     router.push(`/dashboard/team/${team_id}`)
   }
 
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      teamName: '',
+    },
+  })
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    createTeamHandler(values.teamName)
+  }
   return (
-    <main className={'p-[5%]'}>
-      <p className='scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl'>
-        Hello, {error ? 'failed to load' : isLoading ? 'loading...' : user?.nickname}
-      </p>
-      <form action={createTeamHandler} method='POST'>
-        <input type='text' name='name' placeholder='Input a team name' />
-        <Button type='submit'>Crate Team</Button>
-      </form>
+    <main className={'min-h-screen px-[10%] py-[5%]'}>
+      <div className='flex flex-col justify-center items-center gap-8'>
+        <p className='scroll-m-20 text-6xl font-extrabold tracking-tight lg:text-8xl'>
+          Hello, {error ? 'failed to load' : isLoading ? 'loading...' : user?.nickname}
+        </p>
+
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className='space-y-8 max-w-[500px] grid grid-cols-3 gap-4'
+          >
+            <FormField
+              control={form.control}
+              name='teamName'
+              render={({ field }) => (
+                <FormItem className='col-span-2'>
+                  <FormLabel>New Team Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder='Type a new team name here.' {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type='submit'>Crate A Team</Button>
+          </form>
+        </Form>
+      </div>
+      <Separator className='my-[5%]' />
       <div className='text-lg font-semibold'>
         or, you can join the team that you already joined in.
       </div>
-      {res.error ? (
-        <div>failed to load</div>
-      ) : res.data === undefined || res.isLoading ? (
-        'loading...'
-      ) : (
-        res.data.teams.map((team) => (
-          <Button
-            onClick={() => handleSelectTeam(team.id, team.team_boards, team.boards[0])}
-            key={team.id}
-          >
-            {team.name}
-          </Button>
-        ))
-      )}
+      <ScrollArea>
+        <div className='my-5 flex space-x-5'>
+          {res.error ? (
+            <div>failed to load</div>
+          ) : res.data === undefined || res.isLoading ? (
+            'loading...'
+          ) : (
+            res.data.teams.map((team) => (
+              <Card
+                key={team.id}
+                onClick={() =>
+                  handleSelectTeam(team.id, team.team_boards, team.boards[0])
+                }
+                className='w-[350px] shadow hover:text-card hover:bg-card-foreground/90 hover:cursor-pointer transition-colors'
+              >
+                <CardHeader className='grid grid-cols-6 grid-rows-2 gap-2'>
+                  <Avatar className='col-span-1'>
+                    <AvatarFallback>{team.name[0]}</AvatarFallback>
+                  </Avatar>
+                  <CardTitle className='col-span-5'>{team.name}</CardTitle>
+                  <CardDescription className='col-span-6'>
+                    Owned by {team.admin_user.name} and {team.team_members.length - 1}{' '}
+                    others
+                  </CardDescription>
+                </CardHeader>
+              </Card>
+            ))
+          )}
+        </div>
+        <ScrollBar orientation='horizontal' />
+      </ScrollArea>
     </main>
   )
 }
